bd2cf743c875d475e3c616f73ea3c79469fa697b
zabaanto <antonin.zaba@centrum.cz>
2015-02-23 12:56:37 +0100

    Fixed and rewrite file_to_string

--- searching_algorithms/other_functions.cpp
+++ searching_algorithms/other_functions.cpp
@@ -3,15 +3,21 @@
 string file_to_string(string filename) {
 	
 	string text = "";
-	ifstream file_stream (filename, ifstream::binary);
+	ifstream file_stream (filename, ifstream::in);
 
 	if (file_stream.is_open()) {
-
-		int count_char = 0;
-		string row;
 		
-		while ( getline(file_stream, row))
-			text += row.substr(0, row.length()-1);
+		/* GET FILE SIZE */
+		file_stream.seekg(0, file_stream.end);
+		int file_size = file_stream.tellg();
+		file_stream.seekg(0, file_stream.beg);
+
+		/* FILE to STRING */
+		// without newlines (13, 10)
+		char current_char;
+		while (file_stream >> noskipws >> current_char) 
+			if ( (int(current_char) != 13) & (int(current_char) != 10) )
+				text += current_char;
 
 	}
 	else {
