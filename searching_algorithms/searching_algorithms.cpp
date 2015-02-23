116f2c23e8a3f0a282224c3b9c1054d09cd9d8ba
zabaanto <antonin.zaba@centrum.cz>
2015-02-23 19:34:51 +0100

    KMP algorithm - comments

--- searching_algorithms/searching_algorithms.cpp
+++ searching_algorithms/searching_algorithms.cpp
@@ -4,7 +4,7 @@
 void naive_algorithm(string text, string sample)
 {
 	int n = text.length();
-	int m = sample.length();
+	int m = sample.length();	
 
 	/* ITTERATION INSIDE TEXT */
 	for (int main_index = 0; main_index <= n - m; main_index++)
@@ -13,7 +13,7 @@ void naive_algorithm(string text, string sample)
 		
 		// WHILE compare is SUCCEDEing .. only looking for first m-elements in SAMPLE; compare element in TEXT and SAMPLE
 		while ( (control_index <= m-1) && (text[main_index + control_index] == sample[control_index]) )
-			control_index++;  // will be next position to control
+			control_index++;  // next loop looking for next element in SAMPLE
 		
 		// IF all SAMPLE elements checked
 		if (control_index >= m)
@@ -25,36 +25,35 @@ void naive_algorithm(string text, string sample)
 /* KMP ALGORITHM */
 void kmp_algorithm(string text, string sample)
 {
-	int n = text.length();
-	int m = sample.length();
+	int n = text.length(), m = sample.length();
 
 	vector<int> sample_info = kmp_sample_preprocess(sample);  // sample preprocessing
-	int control_index = 0;
-	int main_index = 0;
+	int control_index = 0, main_index = 0;
 
-	/*for (int number: sample_info)
-		cout << number << " ";*/
-
+	/* ITERATION INSIDE TEXT */
 	while (main_index <= n - m + 1)
 	{
+		// TEXT[i] vs SAMPLE[j] .. compare SUCCEDE
 		if (text[main_index] == sample[control_index])
 		{
-			control_index++;
+			control_index++;  // next loop looking for next element in SAMPLE
 
+			// all SAMPLE elements checked
 			if (control_index >= m)
 			{
 				cout << "Word found on position: " << main_index - control_index + 1 << endl;
-				control_index = sample_info[control_index - 1];
+				control_index = sample_info[control_index - 1];  // dont forget for already checked subSAMPLES
 			}
 
-			main_index++;
+			main_index++; // you could move on
 		}
+		// TEXT[i] vs SAMPLE[j] .. compare NOT SUCCEDE
 		else
 		{
-			if (control_index == 0)
-				main_index++;
-			else
-				control_index = sample_info[control_index - 1];
+			// first SAMPLE element doesnt fetch => no subSAMPLES, you could move on
+			if (control_index == 0) main_index++;
+			// otherwise dont forget for already checked subSAMPLES, you CANT move on!
+			else control_index = sample_info[control_index - 1];
 		}
 	}
 
@@ -66,12 +65,11 @@ vector<int> kmp_sample_preprocess(string sample)
 	sample_info[0] = 0;
 	int element_in_a_row = 0;
 
+	// ITERATION INSIDE SAMPLE
 	for (int main_index = 1; main_index < sample.length(); main_index++)
 	{
-		if (sample[element_in_a_row] == sample[main_index])
-			element_in_a_row++;
-		else
-			element_in_a_row = 0;
+		if (sample[element_in_a_row] == sample[main_index]) element_in_a_row++;	 // element in a row
+		else element_in_a_row = 0;  // otherwise start over
 
 		sample_info[main_index] = element_in_a_row;  // save info
 	}
